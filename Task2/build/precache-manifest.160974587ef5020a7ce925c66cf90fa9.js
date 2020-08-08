2b4e35e2ac16081ca3b001af17d1e14d4d3b68d5
maitri <62336925+maitri-cpu@users.noreply.github.com>
2020-08-08 13:56:51 +0530

    Add files via upload

---
+++ Task2/build/precache-manifest.160974587ef5020a7ce925c66cf90fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "157758829c4d9767d055",
+    "url": "/static/css/main.4fb94936.chunk.css"
+  },
+  {
+    "revision": "157758829c4d9767d055",
+    "url": "/static/js/main.15775882.chunk.js"
+  },
+  {
+    "revision": "3e6274beaba09907f7f0",
+    "url": "/static/css/1.b4e39b93.chunk.css"
+  },
+  {
+    "revision": "3e6274beaba09907f7f0",
+    "url": "/static/js/1.3e6274be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7635b78f80ea0e89875e677ce857ce0",
+    "url": "/index.html"
+  }
+];
